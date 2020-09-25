0694bb05ebb13201d8e4988452c3aaaed67eacf1
Gendalf4ever <60482216+Gendalf4ever@users.noreply.github.com>
2020-09-25 16:14:30 +0300

    some tests with webview

--- lib/test.dart
+++ lib/test.dart
@@ -10,28 +10,14 @@ class _TestState extends State<Test> {
   final Completer<WebViewController> _webcontroller = Completer<WebViewController>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Test"),
-    ),
-    body:  WebView(
-                  initialUrl: "http://google.com",
-                  javascriptMode: JavascriptMode.unrestricted,
-                  onWebViewCreated: (WebViewController webcontroller){
-                    _webcontroller.complete(webcontroller);    
-                  },
-                ),
-                floatingActionButton: FutureBuilder<WebViewController>(
-                 future: _webcontroller.future,
-                 builder: (BuildContext context,
-                 AsyncSnapshot<WebViewController> controller){
-                   if (controller.hasData) {
-                     return FloatingActionButton(
-                       child: Icon(Icons.ac_unit), onPressed: (){
-                         controller.data.loadUrl("http://google.com");
-                       },
-                     );
-                   }
-                 },
-                ),
+return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter WebView example'),
+      ),
+      body: const WebView(
+        initialUrl: 'https://pm.tada.team',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
     );
      
   }
